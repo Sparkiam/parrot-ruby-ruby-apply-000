ff7d9cb423cdde96df8a3ae7448edfbbd62c091f
Dell Wilson <warren.wilsonjr89@gmail.com>
2019-11-08 18:32:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!" 
+else puts "Pretty bird! "
+  makes_noise = "Squawk!"
+end
+
+return des_bird 
